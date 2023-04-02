221e1c3d083c2b5b7bbba38a5cabf63d9a990d91
FaiyadRahman <faiyad.r@yahoo.com>
2023-04-02 00:29:21 -0600

    add minWidth to images

--- client/src/components/common/PropertyCard.tsx
+++ client/src/components/common/PropertyCard.tsx
@@ -24,12 +24,12 @@ const PropertyCard = ({
             to={`/properties/show/${id}`}
             sx={{
                 maxWidth: "330px",
+                minWidth: "330px",
                 padding: "10px",
                 "&:hover": {
                     boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.6)",
                 },
                 cursor: "pointer",
-               
             }}
             elevation={0}
         >
